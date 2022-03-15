519ec23ca7ac236c6ba8527185b989e96fe6c960
manoji-s <101625795+manoji-s@users.noreply.github.com>
2022-03-15 17:24:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// this will accept all the calls to root URL http://localhost:8080/
+// It will render the index.html available in the Project root directory as a Response
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+// this will accept all the calls to the URL http://localhost:8080/about
+// It will render the about.html available in the Project root directory as a Response
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname+'/about.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080');
+});
